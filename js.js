d8c1f81423a12608b904d152050c4a86d4be227d
Yurii Stupnytskyi <wedmezin@gmail.com>
2018-09-03 18:25:20 +0300

    hour4-8

--- js.js
+++ js.js
@@ -3,10 +3,9 @@ var elNumber2 = document.getElementById("num2");
 var elCalculate = document.getElementById("calculate");
 var elResult = document.getElementById("num3");
 var select = document.getElementById("selec");
-var value = select.options[select.selectedIndex].value;
-var el = document.getElementById("elem");
+var historyDiv = document.getElementById("history");
 
-	elCalculate.onclick = function(event) {
+	elCalculate.onclick = function() {
 		var num1 = parseInt(elNumber1.value, 10);
 		var value = select.options[select.selectedIndex].value;
 		var num2 = parseInt(elNumber2.value, 10);
@@ -24,8 +23,21 @@ var el = document.getElementById("elem");
 	
 			var listResults = (num1 + value + num2 + "=" + elResult.value);
 			console.log(listResults);
-			el.appendChild(document.createTextNode(listResults +  "\n"));
-						
+
+			var historyEl = document.createElement("li");
+			historyEl.className = "hist";
+			historyEl.appendChild(document.createTextNode(listResults));
+
+			var butt = document.createElement("button");
+			butt.appendChild(document.createTextNode("delete"));
+			
+				butt.onclick = function(event){
+					var liDel = event.target.parentNode;
+					historyDiv.removeChild(liDel);
+				}
+			historyEl.appendChild(butt);
+	
+			historyDiv.appendChild(historyEl);
 		}
 
 
